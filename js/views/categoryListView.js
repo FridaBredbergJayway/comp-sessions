e124a79a05d0481bac5f42b56c993db0bc1e0710
frida <frida.bredberg@jayway.com>
2015-12-08 10:15:46 +0100

    show categories

--- js/views/categoryListView.js
+++ js/views/categoryListView.js
@@ -1,14 +1,13 @@
 /**
  * Created by frida on 11/27/15.
  */
-/*
 var CategoryListView = Backbone.View.extend({
     el: '#category-list',
     model: categoryList,
     initialize: function() {
         categoryList.bind('add', this.add, this);
         categoryList.fetch(); // Loads list from local storage
-        var view = new categoryView({model: Category});
+        var view = new views.CategoryView({model: models.Category});
         $('#list').append(view.render().el);
     },
     render: function () {
@@ -18,4 +17,3 @@ var CategoryListView = Backbone.View.extend({
 
 var categoryListView = new CategoryListView();
 
-*/
--- js/views/categoryView.js
+++ js/views/categoryView.js
@@ -2,20 +2,18 @@
  * Created by frida on 11/27/15.
  */
 
-/*var CategoryView = Backbone.View.extend({
-    model: Category,
+views.CategoryView = Backbone.View.extend({
+    model: models.Category,
     el: '.cat-item',
     template: _.template($('#category-template').html()),
     initialize: function () {
-        this.model = Category;
         this.model.bind('change', this.render, this); //re-render on change
-        this.render();
     },
     render: function () {
         console.log(this.model);
-        this.$el.html(this.template(this.model.toJSON(),{categories: categoryList}));
+        this.$el.html(this.template({categories: categoryList}));
         return this;
     }
 });
 
-var categoryView = new CategoryView();*/
+var categoryView = new views.CategoryView();
