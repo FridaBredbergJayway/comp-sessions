b8fa6488040ee9e6716bf5f051aed26cf25bb1b7
frida <frida.bredberg@jayway.com>
2015-12-09 13:59:38 +0100

    changed collection.on

--- js/views/categoryListView.js
+++ js/views/categoryListView.js
@@ -1,12 +1,14 @@
 /**
  * Created by frida on 11/27/15.
  */
+'use strict';
+
 views.CategoryListView = Backbone.View.extend({
     collection: null,
     el: '#category-list',
     initialize: function() {
         var self = this;
-        this.collection.on("change update add", this.render, this);
+        this.collection.on('add', this.render, this);
         this.collection.fetch({
             success: function() {
                 console.log('fetch done ', self.collection);
