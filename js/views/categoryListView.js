712c85d5cbe51b98985e34c2db15a78e415df901
frida <frida.bredberg@jayway.com>
2015-12-10 14:19:58 +0100

    filter function on sessionList

--- js/views/categoryListView.js
+++ js/views/categoryListView.js
@@ -4,41 +4,42 @@
 views.CategoryListView = Backbone.View.extend({
     collection: null,
     el: '#category-list',
-    initialize: function() {
+    initialize: function () {
         var self = this;
         this.collection.on('add', this.render, this);
         this.collection.on('change:marked', this.filter, this);
         /*this.collection.fetch({
-            success: function() {
-                console.log('fetch done ', self.collection);
-            }
-        });*/
+         success: function() {
+         console.log('fetch done ', self.collection);
+         }
+         });*/
         this.render();
     },
     render: function () {
         var self = this;
         this.$el.empty();
-        this.collection.each(function(model){
+        this.collection.each(function (model) {
             var view = new views.CategoryView({model: model});
             self.$el.append(view.render().el);
         });
         return this;
     },
-    filter: function() {
+    filter: function () {
         console.log('changed mark');
         var markedCategories = this.collection.where({marked: true});
-        if(markedCategories.length === 0) { 
+        if (markedCategories.length === 0) {
             this.render();
-         } else this.showFiltered(markedCategories);
+        } else this.showFiltered(markedCategories);
     },
-    showFiltered: function(array) {
+    showFiltered: function (array) {
         for (var i = 0; i < array.length; i += 1) {
             var category = array[i].get('category');
             var sessions = sessionList.where({category: category});
-            for(var j = 0; j < sessions.length; j += 1) {
-                sessions[j].set('selected', true);
+            for (var j = 0; j < sessions.length; j += 1) {
+                var model = sessions[j];
+                console.log(model);
+                model.set('selected', true);
             }
-            console.log(sessionList);
         }
     }
 });
--- js/views/sessionListView.js
+++ js/views/sessionListView.js
@@ -3,7 +3,8 @@ views.SessionListView = Backbone.View.extend({
     el: '#list',
     initialize: function () {
         var self = this;
-        this.collection.on('add change', this.render, this);
+        this.collection.on('add', this.render, this);
+        this.collection.on('change', this.filter, this)
         /*this.collection.fetch({
             success: function() {
                 console.log('fetch done ', self.collection);
@@ -19,7 +20,13 @@ views.SessionListView = Backbone.View.extend({
             self.$el.append(view.render().el);
         });
         return this;
+    },
+
+    filter: function() {
+       console.log('filter');
     }
+
+
 });
 
 var sessionListView = new views.SessionListView({collection: sessionList});
